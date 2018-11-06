21ee7243316b18b877293b61ec2696f3eaf73c11
Brittney <britpiwowarski@gmail.com>
2018-11-05 19:56:12 -0500

    Added Deck and Prospector code
    The deck class will interpret the info in the DecXML file. The prospector class will manage the overall game.

--- Prospector Solitaire/Assets/__Scripts/Prospector.cs
+++ Prospector Solitaire/Assets/__Scripts/Prospector.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Prospector : MonoBehaviour {
+    static public Prospector S;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [Header("Set in Inspector")]
+    public TextAsset deckXML;
+
+    [Header("Set Dynamically")]
+    public Deck deck;
+
+    void Awake(){
+        S = this; // Set up a Singleton for Prospector    
+    }
+
+    void Start()
+    {
+        deck = GetComponent<Deck>(); // Get the Deck
+        deck.InitDeck(deckXML.text); //Pass DeckXML to it
+    }
 }
