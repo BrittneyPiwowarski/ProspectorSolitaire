481c5501bb7f9e2d328fd318a4e86725becd2bd9
Brittney <britpiwowarski@gmail.com>
2018-11-10 17:43:19 -0500

    Moved cards into basic Prospector Layout
    When the game is played, the cards now appear in the standard Prospector Solitare layout. There are still a few issues with formatting that need to be addressed, but the basic skeleton now appears.

---
+++ Prospector Solitaire/Assets/__Scripts/CardProspector.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum eCardState{
+    drawpile,
+    tableau,
+    target,
+    discard
+}
+
+public class CardProspector : Card { // CardProspector extends Card
+
+    [Header("Set Dynamically: CardProspector")]
+    public eCardState state = eCardState.drawpile;
+    // This hiddenBy list stores which other cards will keep this one face down
+    public List<CardProspector> hiddenby = new List<CardProspector>();
+    // This layoutID matches this card to the tablear XML if its a tableau card
+    public int layoutID;
+    // The SlotDef class stores info pulled from the Layout XML <slot>
+    public SlotDef slotDef;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Prospector Solitaire/Assets/__Scripts/Prospector.cs
+++ Prospector Solitaire/Assets/__Scripts/Prospector.cs
@@ -10,10 +10,18 @@ public class Prospector : MonoBehaviour {
     [Header("Set in Inspector")]
     public TextAsset deckXML;
     public TextAsset layoutXML;
+    public float xOffset = 3;
+    public float yOffset = -2.5f;
+    public Vector3 layoutCenter;
 
     [Header("Set Dynamically")]
     public Deck deck;
     public Layout layout;
+    public List<CardProspector> drawPile;
+    public Transform layoutAnchor;
+    public CardProspector target;
+    public List<CardProspector> tableau;
+    public List<CardProspector> discardPile;
 
     void Awake(){
         S = this; // Set up a Singleton for Prospector    
@@ -25,13 +33,57 @@ public class Prospector : MonoBehaviour {
         deck.InitDeck(deckXML.text); //Pass DeckXML to it
         Deck.Shuffle(ref deck.cards); // This shuffles the deck by reference
 
-        //Card c;
-        //for (int cNum = 0; cNum< deck.cards.Count; cNum++){
-        //    c = deck.cards[cNum];
-        //    c.transform.localPosition = new Vector3((cNum % 13) * 3, cNum / 13 * 4, 0);
-        //}
-
         layout = GetComponent<Layout>();
         layout.ReadLayout(layoutXML.text);
+
+        drawPile = ConvertListCardsTOListCardProspectors(deck.cards);
+        LayoutGame();
+    }
+
+    List <CardProspector> ConvertListCardsTOListCardProspectors(List<Card> lCD){
+        List<CardProspector> lCP = new List<CardProspector>();
+        CardProspector tCP;
+        foreach (Card tCD in lCD){
+            tCP = tCD as CardProspector;
+            lCP.Add(tCP);
+        }
+        return lCP;
+    }
+
+    // The Draw function will pull a single card from the drawPile and return it
+    CardProspector Draw(){
+        CardProspector cd = drawPile[0];
+        drawPile.RemoveAt(0);
+        return cd;
+    }
+
+    // LayoutGame() positions the initial tableau of cards, AKA the "Mine"
+    void LayoutGame(){
+        // Create an empty GameObject to serve as an anchor for the tableau
+        if (layoutAnchor == null){
+            GameObject tGO = new GameObject("_LayoutAnchor");
+            layoutAnchor = tGO.transform;
+            layoutAnchor.transform.position = layoutCenter;
+        }
+
+        CardProspector cp;
+        // Follow the layout
+        foreach (SlotDef tSD in layout.slotDefs){ 
+            cp = Draw(); // Pull a card from the top (beginning) of the draw pile
+            cp.faceUp = tSD.faceUp; // Set its faceUp to the value in SlotDef
+            cp.transform.parent = layoutAnchor; // make its parent layoutAnchor
+            // Set the localPosition of the card based on slotDef
+            cp.transform.localPosition = new Vector3(
+                layout.multiplier.x * tSD.x,
+                layout.multiplier.y * tSD.y,
+                -tSD.layerID);
+            cp.layoutID = tSD.id;
+            cp.slotDef = tSD;
+
+            // CardProspectors in the tableau have the state CardState.tableau
+            cp.state = eCardState.tableau;
+
+            tableau.Add(cp);
+        }
     }
 }
